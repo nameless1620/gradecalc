79a6d54f495cb8f2aeb62b825285b7fa186a0ebd
Joshika Selvaraj <joshika.selvaraj@gmail.com>
2021-02-28 13:32:16 -0500

    added hello name method to sandbox

--- src/main/java/com/nameless1620/gradecalc/ui/views/sandbox/JoshikaSandboxView.java
+++ src/main/java/com/nameless1620/gradecalc/ui/views/sandbox/JoshikaSandboxView.java
@@ -2,9 +2,12 @@ package com.nameless1620.gradecalc.ui.views.sandbox;
 
 import com.nameless1620.gradecalc.ui.MainLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
 
 @PageTitle("GradeCalc | Joshika Sandbox")
 @Route(value = "joshikasandbox", layout = MainLayout.class)
@@ -13,8 +16,15 @@ public class JoshikaSandboxView extends VerticalLayout {
     public JoshikaSandboxView()
     {
         addClassName("joshikasandbox-view");
+        TextField hello = new TextField();
         Button button = new Button("Joshika!");
-        add(button);
+        button.addClickListener(click -> add(new Paragraph(helloName(hello.getValue()))));
+        add(hello, button);
+
+    }
+
+    private String helloName(String name) {
+        return "Hello "+name+"!";
 
         //Description of resource and why to use it
         //Link to resource
@@ -25,5 +35,6 @@ public class JoshikaSandboxView extends VerticalLayout {
         //Description of resource and why to use it
         //Link to resource
     }
+
 
 }
