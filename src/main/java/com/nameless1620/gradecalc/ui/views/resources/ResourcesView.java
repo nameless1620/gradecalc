b17ab38a45e4e4f110c0aeb3c45836289ae8dc18
Joshika Selvaraj <joshika.selvaraj@gmail.com>
2021-04-17 12:15:38 -0400

    added description

--- src/main/java/com/nameless1620/gradecalc/ui/views/resources/ResourcesView.java
+++ src/main/java/com/nameless1620/gradecalc/ui/views/resources/ResourcesView.java
@@ -1,15 +1,33 @@
 package com.nameless1620.gradecalc.ui.views.resources;
 
 import com.nameless1620.gradecalc.ui.MainLayout;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import sun.awt.image.ImageWatched;
 
 @PageTitle("GradeCalc | Resources")
 @Route(value = "resources", layout = MainLayout.class)
 public class ResourcesView extends VerticalLayout {
 
     public ResourcesView(){
+
+//      TextArea textArea = new TextArea("Do you need help finding extra resources? Here are some resources you can use:");
+//      textArea.setPlaceholder("Write here ...");
+        //TextField textField = new TextField("Do you need help finding extra resources? Here are some resources you can use:");
+        Paragraph paragraph = new Paragraph("Do you need help finding extra resources? Here are some resources you can use:");
+        //Label label = new Label("Do you need help finding extra resources? Here are some resources you can use:");
+
+        add(paragraph);
+
+        /* Textual link
+        ImageWatched.Link link = new ImageWatched.Link("Click Me!",
+                new ExternalResource("http://vaadin.com/"));
+        */
 
     }
 }
