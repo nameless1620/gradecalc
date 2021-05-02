8dd40ca50094493e10432c8f31ad3ab74958eab8
Joshika Selvaraj <joshika.selvaraj@gmail.com>
2021-05-02 11:40:13 -0400

    resources

--- src/main/java/com/nameless1620/gradecalc/ui/views/resources/ResourcesView.java
+++ src/main/java/com/nameless1620/gradecalc/ui/views/resources/ResourcesView.java
@@ -1,13 +1,17 @@
 package com.nameless1620.gradecalc.ui.views.resources;
 
 import com.nameless1620.gradecalc.ui.MainLayout;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteConfiguration;
 import sun.awt.image.ImageWatched;
 
 @PageTitle("GradeCalc | Resources")
@@ -18,24 +22,21 @@ public class ResourcesView extends VerticalLayout {
 
 //      TextArea textArea = new TextArea("Do you need help finding extra resources? Here are some resources you can use:");
 //      textArea.setPlaceholder("Write here ...");
+
         //TextField textField = new TextField("Do you need help finding extra resources? Here are some resources you can use:");
         Paragraph paragraph = new Paragraph("Do you need help finding extra resources? Here are some resources you can use:");
         //Label label = new Label("Do you need help finding extra resources? Here are some resources you can use:");
-
         add(paragraph);
 
-        /* Textual link
-        ImageWatched.Link link = new ImageWatched.Link("Click Me!",
-                new ExternalResource("http://vaadin.com/"));
-        */
+        Anchor link1 = new Anchor("https://www.khanacademy.org/", "Khan Academy");
+        add(link1);
 
+        Anchor link2 = new Anchor("https://www.quizlet.com/", "Quizlet");
+        add(link2);
+
+        Anchor link3 = new Anchor("https://www.collegeboard.org/", "Collegeboard");
+        add(link3);
     }
 }
-// Link link = new link( "Click Me!,
-//            new ExternalResource("http;//vaadin.com/")
-/*
-This is used to open a URL in a new tab
 
-link.setIcon(new ThemeResource("icons/external-link.png"));
-link.addStyleName("icon-after-caption");
- */
+
