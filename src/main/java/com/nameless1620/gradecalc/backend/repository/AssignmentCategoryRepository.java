d4c4d9e8fb16a3939d0798f8991b6dae285a0750
Korvyn Dornseif <korvyn@gmail.com>
2021-05-31 11:23:17 -0600

    deleted additional unnecessary files

--- src/main/java/com/nameless1620/gradecalc/backend/repository/AssignmentCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nameless1620.gradecalc.backend.repository;
-
-import com.nameless1620.gradecalc.backend.entity.AssignmentCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentCategoryRepository extends JpaRepository<AssignmentCategory, Long> {
-}
--- src/main/java/com/nameless1620/gradecalc/backend/repository/AssignmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nameless1620.gradecalc.backend.repository;
-
-import com.nameless1620.gradecalc.backend.entity.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-}
